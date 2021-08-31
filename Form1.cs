66790dda03d89247fc2d1a6140ebdefab45ef1d2
Mario <57014126+Marioagp@users.noreply.github.com>
2021-08-31 09:15:19 -0600

    Marioagp (#1)
    * Initial visual interface
    
    * GUI simple ...finished
    
    * Some changes at UI and  little bit code, almost nothing

--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,25 @@ namespace ArduinoFrec_Control
         public Form1()
         {
             InitializeComponent();
+            serialPort1.Open(); //inicializa el puerto serie al iniciarse el programa
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void onButton_Click(object sender, EventArgs e)
+        {
+            //enviar datos al arduino con el click del botón
+            int div = Convert.ToInt32(numericUpDown1.Value); // se convierte a int el valor que devuelve el numericUpDown que es decimal
+            
+            
         }
     }
 }
