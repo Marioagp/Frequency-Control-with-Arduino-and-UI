bdf68d158bf3fb20669861bd7049c04dcafc0f4c
Mario Annier <mario.annier@gmail.com>
2021-09-02 16:53:40 -0600

    Rearranging the code to work properly

--- Form1.cs
+++ Form1.cs
@@ -27,20 +27,16 @@ namespace ArduinoFrec_Control
         private void onButton_Click(object sender, EventArgs e)
         {
             //enviar datos al arduino con el click del botón
-
-            string div = "I" + Convert.ToInt32(numericUpDown1.Value); //La I para decirle al Arduino que inicie y el valor de 
-                                                                      // ToInt32(numericUpDown1.Value) con el int convertido a string con el 
-                                                                      // valor a cargar
-                                                                      //int div = Convert.ToInt32(numericUpDown1.Value); // se convierte a int el valor que devuelve el numericUpDown que es decimal
-            serialPort1.Write(div);                                   //enviando datos por el puerto serial, el cambio del string to int se hace en el Arduino
+            Numerical_Useful int_to_BCD = new Numerical_Useful(); //creando un objeto de tipo Numerical_Useful
+            string m = int_to_BCD.IntToBinary(Convert.ToInt32(numericUpDown1.Value)); //llamando a la instancia IntToBCD4
+            
+            serialPort1.Write(m);  //enviando datos por el puerto serial, el cambio del string to int se hace en el Arduino
 
             string Val;
             Val = numericUpDown1.Value.ToString();    //convertir a sring la salida decimal de numericDown1
             label_Valor_Div.Text = Val;               //mostrar el valor en el label
-
-            Numerical_Useful int_to_BCD = new Numerical_Useful(); //creando un objeto de tipo Numerical_Useful
-            string m = int_to_BCD.IntToBinary(Convert.ToInt32 (numericUpDown1.Value)); //llamando a la instancia IntToBCD4
             //label_Valor_Div.Text = "I" + m;  //visualizando la salida en el label
+
         }
     }
 
